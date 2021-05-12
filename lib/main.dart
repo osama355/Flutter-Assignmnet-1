0f0ee1c64562ee86d0cd0abf08468197ad871ba9
osama355 <muhammadosama3556@gmail.com>
2021-05-12 15:52:52 +0500

    task completed

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(body: Text('Muhammad Osama muhammadosama3556@gmail.com')),
+    );
+  }
+}
